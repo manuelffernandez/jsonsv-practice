5b613a8ae638490a472774793a04081b197b8cdb
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-02-04 13:43:40 -0300

    integrates RouterProvider within App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { router } from './router';
+import { RouterProvider } from 'react-router-dom';
+
 function App() {
-  return <div className='App'>Hola mundo</div>;
+  return (
+    <div className='App'>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
