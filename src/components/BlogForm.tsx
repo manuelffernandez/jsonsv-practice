8e49953648ab909b59d4277c125f3b8484513032
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-02-04 18:13:20 -0300

    creates BlogForm component

---
+++ src/components/BlogForm.tsx
@@ -0,0 +1,9 @@
+const BlogForm = (): JSX.Element => {
+  return (
+    <>
+      <h1 className='subtitle'>Create a new Blog</h1>
+    </>
+  );
+};
+
+export default BlogForm;
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as BlogList } from './BlogList';
 export { default as Header } from './Header';
 export { default as BlogDetail } from './BlogDetail';
 export { default as PagesSelector } from './PagesSelector';
+export { default as BlogForm } from './BlogForm';
