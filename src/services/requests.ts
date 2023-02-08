b0b3b0f4d10027dc708383c4ac8286e32ff9d1b3
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-02-08 12:17:20 -0300

    adds new request function & refacts 'fetchData'

--- src/services/requests.ts
+++ src/services/requests.ts
@@ -1,23 +1,25 @@
 import { POSTS_URL } from './urls';
 import { APIData, ResponseObject } from '../interfaces';
+import { Post } from '../interfaces/services';
 
-const errorHandler = (err: Error): ResponseObject => {
-  return { isOk: false, text: err.message };
-};
-
-const fetchData = (URL: URL): Promise<ResponseObject> => {
-  return fetch(URL)
+const fetchData = (
+  URL: URL,
+  options?: RequestInit
+): Promise<ResponseObject> => {
+  return fetch(URL, options)
     .then((res: Response): Promise<APIData> | never => {
       if (!res.ok) throw new Error(`${res.status} ${res.statusText}`);
       return res.json();
     })
-    .then(data => {
+    .then((data): ResponseObject => {
       return {
         isOk: true,
         data,
       };
     })
-    .catch(errorHandler);
+    .catch((err: Error): ResponseObject => {
+      return { isOk: false, text: err.message };
+    });
 };
 
 export const getPosts = async () => {
@@ -28,4 +30,14 @@ export const getPost = async (blogId: string) => {
   const POST_URL = new URL(POSTS_URL);
   POST_URL.searchParams.set('id', blogId);
   return await fetchData(POST_URL);
+};
+
+export const addPost = async (newPost: Post) => {
+  const OPTIONS = {
+    method: 'POST',
+    body: JSON.stringify(newPost),
+    headers: { 'Content-Type': 'application/json' },
+  };
+
+  return await fetchData(POSTS_URL, OPTIONS);
 };
