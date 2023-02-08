11c172f72e898d8cb9518f2504062abe380385d6
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-02-08 12:23:08 -0300

    refacts Create body

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,5 +1,17 @@
+import { Link } from 'react-router-dom';
+import { BlogForm } from '../components';
+
 const Create = (): JSX.Element => {
-  return <div></div>;
+  return (
+    <>
+      <div className='pageButtonsContainer'>
+        <Link to={'/blogs'} className='preventDefaultStyle genericButton'>
+          Back
+        </Link>
+      </div>
+      <BlogForm />
+    </>
+  );
 };
 
 export default Create;
