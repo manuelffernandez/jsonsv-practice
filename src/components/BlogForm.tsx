1c51b46ae4522b93bc029da8abf0e60b1fe49630
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-02-08 12:22:48 -0300

    refacts BlogForm body

--- src/components/BlogForm.tsx
+++ src/components/BlogForm.tsx
@@ -1,7 +1,74 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
+import useForm from '../hooks/useForm';
+import { ResponseObject } from '../interfaces';
+import { addPost } from '../services/requests';
+
+interface FormStates {
+  isLoading: boolean;
+}
+
 const BlogForm = (): JSX.Element => {
+  const [inputValues, dispatch] = useForm();
+  const [isLoading, setIsLoading] = useState<FormStates['isLoading']>(false);
+
+  const handleResponse = (res: ResponseObject): void => {
+    setIsLoading(false);
+
+    if (res.isOk) {
+      alert('post completed');
+      dispatch({ type: 'clear' });
+    } else {
+      alert('somethig go wrong' + res.text);
+    }
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    setIsLoading(true);
+    addPost(inputValues).then(handleResponse);
+  };
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>
+  ): void => {
+    dispatch({
+      type: 'changeValue',
+      payload: { inputName: e.target.name, inputValue: e.target.value },
+    });
+  };
+
   return (
     <>
-      <h1 className='subtitle'>Create a new Blog</h1>
+      {isLoading ? (
+        <h2 className='title w600'>Publicando...</h2>
+      ) : (
+        <>
+          <h2 className='subtitle'>Create a new Blog</h2>
+          <form action='' onSubmit={handleSubmit}>
+            <label htmlFor='formTitle_id'>aaaaa</label>
+            <input
+              onChange={handleChange}
+              type='text'
+              name='title'
+              id='formTitle_id'
+              placeholder='Blog title'
+              value={inputValues.title}
+              required
+            />
+            <label htmlFor='formBody_id'>bbbbb</label>
+            <textarea
+              onChange={handleChange}
+              value={inputValues.body}
+              name='body'
+              id='formBody_id'
+              placeholder='Blog description'
+              cols={30}
+              rows={10}
+              required></textarea>
+            <button type='submit'>Submit blog</button>
+          </form>
+        </>
+      )}
     </>
   );
 };
