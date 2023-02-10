91976b272923ef281d9e0ce387bf0746ef480ea7
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-02-10 15:47:43 -0300

    updates the style of 'About' page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -9,26 +9,29 @@ const About = (): JSX.Element => {
           Back
         </Link>
       </div>
-      <div>
-        <h3 className='subtitle'>About this</h3>
-        <p className='about__item w600'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
-          corporis tempore facilis ratione minima sint earum rem nulla autem?
-          Distinctio vel ducimus fugit deleniti, eligendi earum reprehenderit
-          aperiam nisi ipsa. Recusandae inventore quasi non, delectus voluptatem
-          harum magni provident dolores adipisci rerum laudantium ullam, totam
-          nam quidem error. Officia aut exercitationem, accusantium tempore
-          possimus debitis dolor vel quia aliquam molestiae?
-        </p>
-      </div>
-      <div>
-        <h3 className='subtitle'>About that</h3>
-        <p className='about__item w600'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint aliquid
-          molestiae quos similique sequi quibusdam maiores illo ipsam dolorum
-          iusto? Maxime deserunt, corrupti eum similique velit quam molestias
-          quas reiciendis?
-        </p>
+      <div className='paper'>
+        <div className='aboutItem'>
+          <h3 className='subtitle'>About this</h3>
+          <p className='about__item w600'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+            corporis tempore facilis ratione minima sint earum rem nulla autem?
+            Distinctio vel ducimus fugit deleniti, eligendi earum reprehenderit
+            aperiam nisi ipsa. Recusandae inventore quasi non, delectus
+            voluptatem harum magni provident dolores adipisci rerum laudantium
+            ullam, totam nam quidem error. Officia aut exercitationem,
+            accusantium tempore possimus debitis dolor vel quia aliquam
+            molestiae?
+          </p>
+        </div>
+        <div className='aboutItem'>
+          <h3 className='subtitle'>About that</h3>
+          <p className='about__item w600'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+            aliquid molestiae quos similique sequi quibusdam maiores illo ipsam
+            dolorum iusto? Maxime deserunt, corrupti eum similique velit quam
+            molestias quas reiciendis?
+          </p>
+        </div>
       </div>
     </div>
   );
