12ac3f58f0769454a8f46e3e02b30b30b9064843
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-02-10 15:49:56 -0300

    implements pagination in BlogList  by adding Paginator

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -1,24 +1,31 @@
 import { useEffect, useState } from 'react';
 import { ResponseObject, APIBlog, blogsData } from '../interfaces';
 import { getBlogs } from '../services/requests';
-import { BlogItem } from './';
+import { BlogItem, Paginator } from './';
 import { Link } from 'react-router-dom';
 
 interface BlogsStates {
   blogs: Array<APIBlog>;
   error: string;
   isLoading: boolean;
+  currentPage: number;
+  pagesQty: number;
 }
 
 const BlogList = (): JSX.Element => {
   const [blogs, setBlogs] = useState<BlogsStates['blogs']>([]);
   const [error, setError] = useState<BlogsStates['error']>('');
   const [isLoading, setIsLoading] = useState<BlogsStates['isLoading']>(false);
+  const [currentPage, setCurrentPage] = useState<BlogsStates['currentPage']>(1);
+  const [pagesQty, setPagesQty] = useState<BlogsStates['pagesQty']>(1);
+
+  const BLOGS_PER_PAGE = 5;
 
   const handleResponse = (res: ResponseObject<blogsData>) => {
     setIsLoading(false);
 
     if (res.isOk) {
+      setPagesQty(Math.ceil(res.blogsQty / BLOGS_PER_PAGE));
       setBlogs(res.blogs);
       setError('');
     } else {
@@ -26,12 +33,20 @@ const BlogList = (): JSX.Element => {
     }
   };
 
+  const handleChangePage = (pageNumber: number) => {
+    if (pageNumber > pagesQty) {
+      return;
+    } else {
+      setCurrentPage(pageNumber);
+    }
+  };
+
   useEffect(() => {
-    getBlogs().then(res => {
-      setIsLoading(true);
+    setIsLoading(true);
+    getBlogs(currentPage, BLOGS_PER_PAGE).then(res => {
       handleResponse(res);
     });
-  }, []);
+  }, [currentPage]);
 
   const loadContent = () => {
     return !error ? (
@@ -48,11 +63,16 @@ const BlogList = (): JSX.Element => {
           <h2 className='title w600'>Loading...</h2>
         ) : (
           <ul>
-            {blogs.map(blog => (
-              <BlogItem key={blog.id} blog={blog} />
-            ))}
+            {blogs.map(blog => {
+              return <BlogItem key={blog.id} blog={blog} />;
+            })}
           </ul>
         )}
+        <Paginator
+          pagesQty={pagesQty}
+          currentPage={currentPage}
+          handleChange={handleChangePage}
+        />
       </>
     ) : (
       <h1>{error}</h1>
