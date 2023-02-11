462432ae6c8d11fe092c2fd6251f0a63f5477947
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-02-11 18:46:14 -0300

    refacts BlogList

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -2,22 +2,28 @@ import { useEffect, useState } from 'react';
 import { ResponseObject, APIBlog, blogsData } from '../interfaces';
 import { getBlogs } from '../services/requests';
 import { BlogItem, Paginator } from './';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { getLastPageContext } from '../contexts/LastPageContext';
 
 interface BlogsStates {
   blogs: Array<APIBlog>;
   error: string;
   isLoading: boolean;
-  currentPage: number;
+  currentPage: string;
   pagesQty: number;
 }
 
 const BlogList = (): JSX.Element => {
+  const { onPageChange, lastPageNumber } = getLastPageContext();
+
   const [blogs, setBlogs] = useState<BlogsStates['blogs']>([]);
   const [error, setError] = useState<BlogsStates['error']>('');
   const [isLoading, setIsLoading] = useState<BlogsStates['isLoading']>(false);
-  const [currentPage, setCurrentPage] = useState<BlogsStates['currentPage']>(1);
+  const [currentPage, setCurrentPage] =
+    useState<BlogsStates['currentPage']>(lastPageNumber);
   const [pagesQty, setPagesQty] = useState<BlogsStates['pagesQty']>(1);
+
+  const { pageNumber } = useParams();
 
   const BLOGS_PER_PAGE = 5;
 
@@ -25,25 +31,38 @@ const BlogList = (): JSX.Element => {
     setIsLoading(false);
 
     if (res.isOk) {
-      setPagesQty(Math.ceil(res.blogsQty / BLOGS_PER_PAGE));
-      setBlogs(res.blogs);
-      setError('');
+      if (res.blogs.length > 0) {
+        setPagesQty(Math.ceil(res.blogsQty / BLOGS_PER_PAGE));
+        setBlogs(res.blogs);
+        setError('');
+      } else {
+        setError('No blogs founded');
+      }
     } else {
+      // ts assertion
       setError(res.text as string);
     }
   };
 
-  const handleChangePage = (pageNumber: number) => {
-    if (pageNumber > pagesQty) {
+  const handleChangePage = (newPageNumber: number) => {
+    if (newPageNumber > pagesQty) {
       return;
     } else {
-      setCurrentPage(pageNumber);
+      setCurrentPage(newPageNumber.toString());
     }
   };
 
   useEffect(() => {
     setIsLoading(true);
-    getBlogs(currentPage, BLOGS_PER_PAGE).then(res => {
+
+    // ts assertion
+    onPageChange(pageNumber as string);
+
+    // ts assertion
+    setCurrentPage(pageNumber as string);
+
+    // ts assertion
+    getBlogs(pageNumber as string, BLOGS_PER_PAGE.toString()).then(res => {
       handleResponse(res);
     });
   }, [currentPage]);
@@ -55,7 +74,9 @@ const BlogList = (): JSX.Element => {
           <Link to={'/'} className='preventDefaultStyle genericButton'>
             Back
           </Link>
-          <Link to='create' className='preventDefaultStyle genericButton w300'>
+          <Link
+            to='/blogs/create'
+            className='preventDefaultStyle genericButton w300'>
             Add a new blog
           </Link>
         </div>
@@ -75,7 +96,12 @@ const BlogList = (): JSX.Element => {
         />
       </>
     ) : (
-      <h1>{error}</h1>
+      <>
+        <h1 className='subtitle'>{error}</h1>
+        <Link to='/' className='preventDefaultStyle genericButton'>
+          Back to home
+        </Link>
+      </>
     );
   };
 
