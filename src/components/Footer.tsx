96fe2998e6561507223dbc9110d10a0214a92b81
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-02-11 19:45:12 -0300

    creates Footer comp

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+const Footer = (): JSX.Element => {
+  return (
+    <footer className='footer w600'>Created by Manuel Fernandez - 2023</footer>
+  );
+};
+
+export default Footer;
--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,4 @@ export { default as BlogDetail } from './BlogDetail';
 export { default as BlogForm } from './BlogForm';
 export { default as Paginator } from './Paginator';
 export { default as Create } from './Create';
+export { default as Footer } from './Footer';
