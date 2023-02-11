7a3a9eac93d0eee787ee72c916256cd366da66b9
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-02-11 19:45:43 -0300

    updates Header comp

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,15 @@ import { Link } from 'react-router-dom';
 const Header = (): JSX.Element => {
   return (
     <header className='header'>
-      <Link to='/' className='preventDefaultStyle'>
-        <div className='header__brandLogo w600'>Blog Manu</div>
+      <Link to='/' className='preventDefaultStyle header__brandLogo w600'>
+        Blog Manu
       </Link>
+      <a
+        href='https://github.com/manuelffernandez/jsonsv-practice'
+        target='_blank'
+        className='preventDefaultStyle header__repoLink'>
+        Link to the repo
+      </a>
     </header>
   );
 };
